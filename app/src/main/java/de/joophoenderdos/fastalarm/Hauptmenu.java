6258f6d7454cb9d33b9befb0a59749c239ce4b14
DerGamerTv <joophoe@gmail.com>
2022-02-02 12:48:30 +0100

    initial commit

---
+++ app/src/main/java/de/joophoenderdos/fastalarm/Hauptmenu.java
@@ -0,0 +1,4 @@
+package de.joophoenderdos.fastalarm;
+
+public class Hauptmenu {
+}
--- app/src/main/java/de/joophoenderdos/fastalarm/MainActivity.java
+++ app/src/main/java/de/joophoenderdos/fastalarm/MainActivity.java
@@ -3,6 +3,10 @@ package de.joophoenderdos.fastalarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+
+        MaterialButton loginbtn = findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(v -> {
+            if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                //correct
+                Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+            } else
+                //incorrect
+                Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+        });
+
+
     }
 }
